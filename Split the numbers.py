0bd51403cf77eb458746f0af11750bdb222cdd2e
Bhavan007-SB <82212873+Bhavan007-SB@users.noreply.github.com>
2021-04-23 11:43:42 +0530

    Create Split the numbers.py

---
+++ Split the numbers.py
@@ -0,0 +1,4 @@
+num = int(input("Enter the Number: ")) # Get the Number from user
+
+for digits in str(num):                #In the  "for loop" number is spillted and interger is converted into string
+  print((digits),end=" "))             # Here "end" appends the space instead of new link 
