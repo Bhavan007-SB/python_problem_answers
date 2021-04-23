93d72d3db7dc08493cdca67d9354e9927860d421
Bhavan007-SB <82212873+Bhavan007-SB@users.noreply.github.com>
2021-04-23 11:25:41 +0530

    Create MulitiplicationTables.py

---
+++ MulitiplicationTables.py
@@ -0,0 +1,4 @@
+num = int(input("Show the Multiplication table of : ")) #get integer input from user
+
+for i in range(1,21):            #using "for loop" for mupliply 20 times
+  print(i,"X",num, "=", num*i)
